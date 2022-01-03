a335f4402ddf7a6e9792c0c99fd91e5a3d6c32ee
OctoBirb <53840213+OctoBirb@users.noreply.github.com>
2022-01-03 06:50:23 -0500

    added canvas
    how do i commit changes without having to upload files again

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var x = 10
 var xps = 0
+var canvas = document.getElementById('xVis');
+
+if (canvas.getContext) {
+	var ctx = canvas.getContext('2d');
+	ctx.fillStyle = 'rgb(255, 255, 255)'
+	ctx.fillRect(25, 25, 100, 100)
+}
+
 function ix() { x++ }
 window.setInterval(function() {
 	r("xDis", `You have ${x} bytes. (0 KB)`)
