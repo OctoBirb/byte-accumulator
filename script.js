cbb5b6e13164cdae7aa13555c3b9abb50d6a5d71
OctoBirb <53840213+OctoBirb@users.noreply.github.com>
2022-01-03 08:32:46 -0500

    Added saving and loading (for script.js)

--- script.js
+++ script.js
@@ -9,6 +9,23 @@ if (canvas.getContext) {
 }
 
 function ix() { x++ }
+
 window.setInterval(function() {
 	r("xDis", `You have ${x} bytes. (0 KB)`)
 }, 20)
+
+var save = {
+    x: x
+    xps: xps
+}
+function save() {
+    localStorage.setItem("save", JSON.stringify(save))
+}
+function load() {
+    var saveL = JSON.parse(localStorage.getItem("save"));
+    x = savegame.x
+    xps = savegame.xps
+}
+window.setInterval(function() {
+    save()
+}, 15000)
