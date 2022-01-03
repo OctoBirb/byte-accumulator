60f98872735f6d27d691c5e788d0640cf620d857
OctoBirb <53840213+OctoBirb@users.noreply.github.com>
2022-01-03 09:59:56 -0500

    logialog
    a

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 var x = 10
 var xps = 0
 var canvas = document.getElementById('xVis');
+  
+var r = (id, text) => { // useful for incremental updating
+	document.getElementById(id).textContent = text
+}
+var i2 = (x, y) => { // useful for incrementals in general
+	x = x + y
+}
+var i1 = (x) => {x++}
+var cl = (x) => { // useful for anything
+	console.log(x)
+}
 
+function draw() {
 if (canvas.getContext) {
 	var ctx = canvas.getContext('2d');
 	ctx.fillStyle = 'rgb(255, 255, 255)'
-	while (x < 1e100) {
-		ctx.fillRect(25, 25, log(x), log(x))
-	}
-}
+	ctx.fillRect(25, 25, Math.log(x), Math.log(x))
+}}
 
 function ix() { x++ }
-
-window.setInterval(function() {
-	r("xDis", `You have ${x} bytes. (0 KB)`)
-}, 20)
 
 var save = {
 	x: x,
@@ -28,6 +34,13 @@ function load() {
     x = savegame.x
     xps = savegame.xps
 }
+
 window.setInterval(function() {
-    save()
+	r("xDis", `You have ${x} bytes. (0 KB)`)
+	draw()
+	x++
+}, 20)
+
+window.setInterval(function() {
+    localStorage.setItem("save", JSON.stringify(save))
 }, 15000)
